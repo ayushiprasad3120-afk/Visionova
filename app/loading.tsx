1b1aba644a7b53075ce6231c4f9875f4a2ab3200
ayushiprasad3120-afk <ayushiprasad3120@gmail.com>
2026-09-24 04:54:16 +0530

    Delete app directory

--- app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="min-h-[70vh] flex items-center justify-center">
-      <div className="h-10 w-10 rounded-full border-2 border-wine/20 border-t-wine animate-spin" />
-    </div>
-  );
-}
